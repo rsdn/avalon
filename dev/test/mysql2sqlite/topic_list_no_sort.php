ab06c1def6fdf70e573d57da225211592a4a3a6c
Anton Batenev <antonbatenev@yandex.ru>
2011-06-25 20:28:25 +0400

    Добавление тестов из старого svn (чтобы не потерялись)

---
+++ dev/test/mysql2sqlite/topic_list_no_sort.php
@@ -0,0 +1,67 @@
+<?php
+
+/*!
+ * \file
+ * \brief Быстрый тест выбоки списка топиков для форума с включенным кэшем и сортировкой
+ *
+ * $Date: 2009-10-29 18:49:00 +0300 (Чтв, 29 Окт 2009) $
+ * $Author: antonbatenev.ya.ru $
+ * $Revision: 305 $
+ * $URL: svn://opensvn.ru/avalon/test/mysql2sqlite/topic_list_no_sort.php $
+ */
+
+/*
+Количество запросов        : 100000
+Общее время запросов       : 6.032
+Среднее время на запрос    : 0.00006
+Общее время теста          : 7.195
+Время дополнительного кода : 1.163
+*/
+
+$time_test  = microtime(true);
+
+$connection = null;
+
+try
+{
+	$connection = new PDO("sqlite:avalon.db");
+}
+catch (PDOException $e)
+{
+	die ("SQLite connection error\n");
+}
+
+$sql = "
+SELECT
+	`id_message`
+FROM
+	`message_topic`
+WHERE
+	`id_forum` = 8
+ORDER BY
+	`id_message` DESC
+";
+
+$count = 1000000;
+$time_query = 0;
+
+for ($i = 0; $i < $count; $i++)
+{
+	$time_start = microtime(true);
+
+	$query = $connection->query($sql) or die ("sqlite_query: " . $connection->errorInfo());
+
+	$time_query = $time_query + (microtime(true) - $time_start);
+
+	$query->closeCursor();
+}
+
+$time_test = microtime(true) - $time_test;
+
+echo "Количество запросов        : " . strval($count) . "\n";
+echo "Общее время запросов       : " . sprintf("%.3f", $time_query) . "\n";
+echo "Среднее время на запрос    : " . sprintf("%.5f", $time_query / $count) . "\n";
+echo "Общее время теста          : " . sprintf("%.3f", $time_test) . "\n";
+echo "Время дополнительного кода : " . sprintf("%.3f", $time_test - $time_query) . "\n";
+
+?>
---
+++ dev/test/quick_mysql/topic_list_no_cache.php
@@ -0,0 +1,62 @@
+<?php
+
+/*!
+ * \file
+ * \brief Быстрый тест выбоки списка топиков для форума с выключенным кэшем и сортировкой
+ *
+ * $Date: 2009-03-28 10:17:43 +0300 (Сбт, 28 Мар 2009) $
+ * $Author: antonbatenev.ya.ru $
+ * $Revision: 194 $
+ * $URL: svn://opensvn.ru/avalon/test/quick_mysql/topic_list_no_cache.php $
+ */
+
+/*
+Количество запросов        : 1000
+Общее время запросов       : 102.570
+Среднее время на запрос    : 0.103
+Общее время теста          : 102.678
+Время дополнительного кода : 0.108
+*/
+
+$time_test  = microtime(true);
+
+$connection = mysql_connect("localhost", "root", "") or die("mysql_connect: " . mysql_error());
+
+mysql_select_db("avalon", $connection) or die ("mysql_select_db : " . mysql_error($connection));
+
+$sql = "
+SELECT SQL_NO_CACHE
+    `id_message`
+FROM
+    `message_topic`
+WHERE
+    `id_forum` = 8
+ORDER BY
+    `message_date` DESC
+";
+
+$count = 1000;
+$time_query = 0;
+
+for ($i = 0; $i < $count; $i++)
+{
+    $time_start = microtime(true);
+
+    $query = mysql_query($sql, $connection) or die("mysql_query: " . mysql_error($connection));
+
+    $time_query = $time_query + (microtime(true) - $time_start);
+
+    mysql_free_result($query) or die("mysql_free_result: " . mysql_error($connection));
+}
+
+mysql_close($connection);
+
+$time_test = microtime(true) - $time_test;
+
+echo "Количество запросов        : " . strval($count) . "\n";
+echo "Общее время запросов       : " . sprintf("%.3f", $time_query) . "\n";
+echo "Среднее время на запрос    : " . sprintf("%.5f", $time_query / $count) . "\n";
+echo "Общее время теста          : " . sprintf("%.3f", $time_test) . "\n";
+echo "Время дополнительного кода : " . sprintf("%.3f", $time_test - $time_query) . "\n";
+
+?>
